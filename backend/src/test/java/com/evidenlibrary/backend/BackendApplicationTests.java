5ecd7bc2b476861e4da25bbcc501f968f3939a74
Fernando Robla <fernando.robla@eviden.com>
2025-02-17 16:21:43 +0100

    Base para frontend/backend

---
+++ backend/src/test/java/com/evidenlibrary/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evidenlibrary.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
