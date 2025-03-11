d90de1e979dc4adadc319de07b630a48e27233d0
Fernando Robla <fernando.robla@eviden.com>
2025-03-11 11:38:10 +0100

    Arreglos conflictos merge

--- backend/src/main/java/com/evidenlibrary/backend/apirest/model/dao/LibroDao.java
+++ backend/src/main/java/com/evidenlibrary/backend/apirest/model/dao/LibroDao.java
@@ -14,13 +14,9 @@ public interface LibroDao extends JpaRepository<Libro, Long> {
 	//Busqueda
 	public List<Libro> findByTituloContainingIgnoreCaseOrAutores_NombreContainingIgnoreCaseOrGeneros_NombreContainingIgnoreCase(
             String titulo, String autor, String genero);
-<<<<<<< HEAD
+
 	//Ranking
 	@Query("SELECT l FROM Libro l LEFT JOIN l.valoraciones v GROUP BY l.id ORDER BY AVG(v.puntuacion) DESC LIMIT 10")
-=======
-	
-	@Query("SELECT l FROM Libro l ORDER BY l.valoracionMedia DESC LIMIT 10")
->>>>>>> desarrollo
     List<Libro> findTop10MejorValorados();
 
 
--- frontend/src/app/autor/autor.service.ts
+++ frontend/src/app/autor/autor.service.ts
@@ -9,8 +9,8 @@ import { Router } from '@angular/router';
   providedIn: 'root'  //disponible a nivel global
 })
 export class AutorService implements OnInit{
-  private urlEndPoint: string = 'http://localhost:8081/api/autores';
-  private urlEndPoint1: string = 'http://localhost:8081/api/autor'; 
+  private urlEndPoint: string = 'http://localhost:8080/api/autores';
+  private urlEndPoint1: string = 'http://localhost:8080/api/autor'; 
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
   // Creamos un BehaviorSubject para la lista de autores
