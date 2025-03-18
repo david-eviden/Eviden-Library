d3ca6990988c27894e2ec8424cb9b3d96efc8150
maria-amigo <maria.amigo@eviden.com>
2025-03-18 16:31:12 +0100

    detalles-pedido.service

--- frontend/src/app/detalles-pedido/detalles-pedido.service.ts
+++ frontend/src/app/detalles-pedido/detalles-pedido.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { detallesPedido } from './detalles-pedido';
@@ -11,33 +11,10 @@ export class DetallesPedidoService {
 
     private urlEndPoint: string = 'http://localhost:8081/api/detallesPedido'; 
   
-    constructor(private http: HttpClient, private router: Router) {}
-  
-    // Método para obtener el token del localStorage
-    private getToken(): string | null {
-      const token = localStorage.getItem('access_token');
-      if (!token) {
-        this.router.navigate(['/login']);  // Redirigir al login si el token no está presente
-      }
-      return token;
-    }
-  
-    // Método para crear cabeceras con el token
-    private createHeaders(): HttpHeaders {
-      const token = this.getToken();
-      let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    
-      if (token) {
-        headers = headers.append('Authorization', `Bearer ${token}`);
-      } else {
-        console.log('No se encontró token en localStorage');
-      }
-    
-      return headers;
-    }
+    constructor(private http: HttpClient, private router: Router) {}  
 
     getdetallesPedido(): Observable<detallesPedido[]> {
-      return this.http.get(this.urlEndPoint, { headers: this.createHeaders() }).pipe(
+      return this.http.get(this.urlEndPoint).pipe(
   
         // Conversión a detallesPedido (response de Object a detallesPedido[])
         map(response => {
